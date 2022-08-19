f6919e375b069134c157977ee0784a6eb17b1e77
Nikhil Shahi <shniks@gmail.com>
2022-08-19 08:31:08 -0700

    Created feature test for article.

---
+++ spec/features/user_sees_all_articles_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'When a user visits the articles index page' do
+  it 'they see all the articles' do
+    article_1 = Article.create!(title: "Title 1", body: "Body 1")
+    article_2 = Article.create!(title: "Title 2", body: "Body 2")
+
+    visit '/articles'
+
+    expect(page).to have_content(article_1.title)
+    expect(page).to have_content(article_2.title)
+    expect(page).to have_content(article_1.body)
+    expect(page).to have_content(article_2.body)
+  end
+end 
