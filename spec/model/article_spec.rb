55b54a3bff1159435a9373184a0f665fb07ecb0d
Nikhil Shahi <shniks@gmail.com>
2022-08-19 08:23:52 -0700

    renamed description attribute to body

--- spec/model/article_spec.rb
+++ spec/model/article_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   describe 'validations' do
     it { should validate_presence_of :title }
-    it { should validate_presence_of :description }
+    it { should validate_presence_of :body }
   end
-end 
+end
